6ca42a64cb61e04d96e06cc3c0ded10e85f6ae5e
daria9820 <68997093+daria9820@users.noreply.github.com>
2020-07-30 22:14:15 +1000

    Update Oil-003.py

--- Oil-003.py
+++ Oil-003.py
@@ -1,13 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[1]:
-
-
-# Импортируем модули и библиотеки
-
-
-# In[3]:
 
 
 import pandas as pd
@@ -16,19 +7,18 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model
 
 
-# In[5]:
+
 
 
 df = pd.read_excel('gdprussia.xlsx')
 
 
-# In[24]:
+
 
 
 df
 
 
-# In[11]:
 
 
 get_ipython().run_line_magic('matplotlib', 'inline')
@@ -37,13 +27,6 @@ plt.xlabel('oil price (US$)')
 plt.ylabel('GDP, Russia (bln US$)')
 
 
-# In[12]:
-
-
-# Тренируем модель
-
-
-# In[13]:
 
 
 reg = linear_model.LinearRegression()
@@ -51,19 +34,11 @@ reg = linear_model.LinearRegression()
 reg.fit(df[['oilprice']], df.gdp)
 
 
-# In[14]:
-
-
-# Предсказываем
-
-
-# In[15]:
-
 
 reg.predict(df[['oilprice']])
 
 
-# In[16]:
+
 
 
 get_ipython().run_line_magic('matplotlib', 'inline')
@@ -73,19 +48,12 @@ plt.ylabel('GDP, Russia (bln US$)')
 plt.plot(df.oilprice, reg.predict(df[['oilprice']]))
 
 
-# In[18]:
 
 
 reg.predict([[150]])
 
 
-# In[20]:
 
-
-# Модель, предсказывающая ВВП в зависимости от 1) года и 2) от цены на нефть
-
-
-# In[21]:
 
 
 reg = linear_model.LinearRegression()
@@ -93,19 +61,16 @@ reg = linear_model.LinearRegression()
 reg.fit(df[['year','oilprice']], df.gdp)
 
 
-# In[22]:
 
 
 reg.predict(df[['year','oilprice']])
 
 
-# In[23]:
 
 
 reg.predict([[2025,100]])
 
 
-# In[ ]:
 
 
 
